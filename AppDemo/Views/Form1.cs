a1fe215cbdc0d27e7faa84a85394be01fa69d262
1512214@student.hcmus.edu.vn <1512214@student.hcmus.edu.vn>
2019-01-04 21:14:17 +0700

    Fix read data with first time

---
+++ AppDemo/Views/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SEP_framwork.Controllers.HandleController;
+using SEP_framwork.Views.FormData;
+using SEP_framwork.Factory;
+
+namespace AppDemo
+{
+    public partial class Form1 : Form
+    {
+        FormFactory formFactory = new FormFactory();
+        string cnnString = @"Data Source=DESKTOP-GN3V8MM\SQLEXPRESS;Initial Catalog=Test;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Read_Click(object sender, EventArgs e)
+        {
+            BaseForm readForm = formFactory.getForm(SEP_framwork.Factory.typeForm.READ, cnnString, "HocSinh");
+            readForm.ExceptColumns(new string[] { "isDelete" });
+            //readForm.ChangeNameColumns(new Dictionary<string, string>() {
+            //    { "Username", "Tên tài khoản" },
+            //    { "Password", "Mật khẩu" },
+            //    { "HoTen", "Họ và Tên" },
+            //    { "GioiTinh", "Giới tính" }
+            //});
+            readForm.SetupForm();
+            readForm.ShowForm();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            BaseForm addForm = formFactory.getForm(SEP_framwork.Factory.typeForm.ADD, cnnString, "HocSinh");
+            addForm.SetPrimaryKey("id");
+            addForm.SetupForm();
+            addForm.ShowForm();
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            BaseForm updateForm = formFactory.getForm(SEP_framwork.Factory.typeForm.UPDATE, cnnString, "HocSinh");
+            updateForm.ExceptColumns(new string[] { "isDelete" });
+            updateForm.SetPrimaryKey("id");
+            updateForm.SetupForm();
+            updateForm.ShowForm();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            BaseForm deleteForm = formFactory.getForm(SEP_framwork.Factory.typeForm.DELETE, cnnString, "HocSinh");
+            deleteForm.ExceptColumns(new string[] { "isDelete" });
+            deleteForm.SetPrimaryKey("id");
+            deleteForm.SetupForm();
+            deleteForm.ShowForm();
+        }
+    }
+}
---
+++ AppDemo/Views/FormData/BaseForm.cs
@@ -0,0 +1,69 @@
+﻿using SEP_framwork.Controllers.HandleController;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEP_framwork.Views.FormData
+{
+    public class BaseForm
+    {
+        protected DataGridView gridView = new DataGridView();
+        protected string nameTable;
+        protected string primaryKey;
+        protected Button save;
+        protected Button cancel;
+        protected Form form;
+        protected HandleController controllerData;
+
+        public BaseForm(string cnnString, string nameTable)
+        {
+            this.form = new Form();
+            form.Text = "SIMPLE ENTERPRISE FRAMWORK";
+            this.nameTable = nameTable;
+            this.controllerData = new HandleController(cnnString);
+
+            this.save = new Button();
+            this.cancel = new Button();
+
+            this.save.Click += Save_Click;
+            this.cancel.Click += Cancel_Click;
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.form.Close();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            this.clickSave();
+        }
+
+        public void ShowForm()
+        {
+            this.AddTitle();
+            this.form.Show();
+        }
+
+        public void SetupForm()
+        {
+            this.InitializeForm();
+        }
+
+        public void SetPrimaryKey(string key)
+        {
+            primaryKey = key;
+        }
+
+        protected virtual void InitializeForm() { }
+        protected virtual void AddTitle() { }
+        protected virtual void clickSave() { } 
+        public virtual void ExceptColumns(string[] cols) { }
+        public virtual void ChangeNameColumns(Dictionary<string, string> listName) { }
+    }
+}
